bbb3f0787e76cf53089d19007c1c6ade9ed7d878
Elaine Anderson <eparie@bu.edu>
2018-07-01 02:59:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ ORDER BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, pledge.amount FROM users
+"SELECT users.name, users.age, pledges.amount FROM users
 JOIN pledges
 ON user.id = pledges.user_id
 ORDER BY users.name"
